917f509935fccb10da2e4c9a235b7b3160c109ae
Fanat4ebyrekov <81490021+Fanat4ebyrekov@users.noreply.github.com>
2021-10-18 21:49:20 +0300

    1

---
+++ LB4/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LB4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox3_Checked(object sender, RoutedEventArgs e)
+        {
+            checkBox1.IsChecked = false;
+            checkBox2.IsChecked = false;
+        }
+
+        private void checkBox2_Checked(object sender, RoutedEventArgs e)
+        {
+            checkBox3.IsChecked = false;
+            checkBox1.IsChecked = false;
+        }
+
+        private void checkBox1_Checked(object sender, RoutedEventArgs e)
+        {
+
+            checkBox3.IsChecked = false;
+            checkBox2.IsChecked = false;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+        }
+    }
+}
---
+++ LB4/SecondWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LB4
+{
+    /// <summary>
+    /// Логика взаимодействия для SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtboxFontSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtboxFontSize.Text))
+            {
+                txtboxFontSize.Text = "1";
+            }
+
+            if (SliderFontSize != null && Message != null)
+            {
+                var size = Int32.Parse(txtboxFontSize.Text);
+                SliderFontSize.Value = size;
+                Message.FontSize = size;
+            }
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            var next = new SecondWindow();
+            next.ShowDialog();
+        }
+    }
+}
